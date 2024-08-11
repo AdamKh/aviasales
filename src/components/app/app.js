11bdb31aca36aabef2e816427d28b21e2aa86f03
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-08-11 17:23:09 +0300

    ready for programming

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import './app.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  )
+}
